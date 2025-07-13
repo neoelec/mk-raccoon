0c233502149b9e898828cb63379590678fd41b10
YOUNGJIN JOO <neoelec@gmail.com>
2025-07-14 01:57:24 +0900

    gcc_at91: deligate platform specific configurations
    Signed-off-by: YOUNGJIN JOO <neoelec@gmail.com>

--- gcc_at91.mk
+++ gcc_at91.mk
@@ -6,10 +6,11 @@ GCC_AT91_MK_DIR		:= $(shell dirname $(GCC_AT91_MK_FILE))
 
 # Chip & board used for compilation
 # (can be overriden by adding CHIP=chip and BOARD=board to the command-line)
-CHIP			?= at91sam7s256
 BOARD			?= at91sam7s-ek
 PLATFORM		?= at91sam7s-ek
 PROGMEM			?= flash
+
+include $(GCC_AT91_MK_DIR)/mk/at91/$(PLATFORM).mk
 
 # Compile for all memories available on the board (this sets $(MEMORIES))
 include $(AT91LIB)/boards/$(PLATFORM)/board.mak
@@ -33,16 +34,12 @@ OBJDIR			:= obj/$(PROGMEM)
 # Append OBJ and BIN directories to output filename
 OUTPUT			:= $(addprefix $(BINDIR)/, $(TARGET)-$(BOARD)-$(CHIP))
 
-VPATH			+= $(AT91LIB)/boards/$(BOARD)
-VPATH			+= $(AT91LIB)/boards/$(PLATFORM)
 VPATH			+= $(AT91LIB)/utility
 
-EXTRAINCDIRS		+= $(AT91LIB)/boards/$(BOARD)
-EXTRAINCDIRS		+= $(AT91LIB)/boards/$(PLATFORM)
 EXTRAINCDIRS		+= $(AT91LIB)
 
-CSRCS			+= board_lowlevel.c board_memories.c
-ASRCS			+= board_cstartup.S
+CSRCS			+=
+ASRCS			+=
 
 #---------------- Compiler Options ----------------
 #  -g*:          generate debugging information
@@ -54,7 +51,10 @@ ASRCS			+= board_cstartup.S
 CFLAGS			+= -D$(CHIP)
 CFLAGS			+= -D$(PROGMEM)
 CFLAGS			+= -DTRACE_LEVEL=$(TRACE_LEVEL)
-CFLAGS			+= -Wall -mlong-calls -ffunction-sections --param=min-pagesize=0
+CFLAGS			+= -Wall
+CFLAGS			+= -mlong-calls
+CFLAGS			+= -ffunction-sections
+CFLAGS			+= --param=min-pagesize=0
 
 #---------------- Assembler Options ----------------
 #  -Wa,...:   tell GCC to pass this to the assembler.
@@ -67,49 +67,15 @@ CFLAGS			+= -Wall -mlong-calls -ffunction-sections --param=min-pagesize=0
 #       dump that will be displayed for a given single line of source input.
 ASFLAGS			+= -D__ASSEMBLY__
 
-#---------------- Library Options ----------------
-# None pritf options
-__PRINTF_LIB_NONE	:=
-
-# Minimalistic printf version
-__PRINTF_LIB_MIN	:= -Wl,-u,vfprintf -lprintf_min
-
-# Floating point printf version (requires MATH_LIB = -lm below)
-__PRINTF_LIB_FLOAT	:= -Wl,-u,vfprintf -lprintf_flt
-
-# If this is left blank, then it will use the Standard printf version.
-PRINTF_LIB		?= NONE
-
-# None scanf options
-__SCANF_LIB_NONE	:=
-
-# Minimalistic scanf version
-__SCANF_LIB_MIN		:= -Wl,-u,vfscanf -lscanf_min
-
-# Floating point + %[ scanf version (requires MATH_LIB = -lm below)
-__SCANF_LIB_FLOAT	:= -Wl,-u,vfscanf -lscanf_flt
-
-# If this is left blank, then it will use the Standard scanf version.
-SCANF_LIB		?= NONE
-
-# Math library options
-__MATH_LIB_FALSE	:=
-__MATH_LIB_TRUE		:= -lm
-
-MATH_LIB		?= FALSE
-
-# Linker Script
-LD_SCRIPT		:= $(AT91LIB)/boards/$(PLATFORM)/$(CHIP)/$(PROGMEM).lds
-
 #---------------- Linker Options ----------------
 #  -Wl,...:     tell GCC to pass this to linker.
 #    -Map:      create map file
 #    --cref:    add cross reference to  map file
 LDFLAGS			+= -T"$(LD_SCRIPT)"
-LDFLAGS			+= -nostartfiles -Wl,--gc-sections
-LDFLAGS			+= $(__PRINTF_LIB_$(PRINTF_LIB))
-LDFLAGS			+= $(__SCANF_LIB_$(SCANF_LIB))
-LDFLAGS			+= $(__MATH_LIB_$(MATH_LIB))
+LDFLAGS			+= -nostartfiles
+LDFLAGS			+= -Wl,--gc-sections
+LDFLAGS			+= -static
+LDFLAGS			+= -Wl,--start-group -lc -lm -Wl,--end-group
 
 include $(GCC_AT91_MK_DIR)/mk/base_gcc.mk
 
@@ -124,5 +90,11 @@ bin: $(OUTPUT).bin
 	$(OBJCOPY) -O binary $< $@
 
 .PHONY: elf
+
+JLINKCHIP		:= $(shell echo $(CHIP) | tr a-z A-Z)
+JFLASHBASE		:= $(BASE_$(PROGMEM))
+
+JFLASHFLAGS		+= -openprj $(JLINKBIN)/Samples/JFlash/ProjectFiles/Atmel/$(JLINKCHIP).jflash
+JLINKGDBFLAGS		+= -endian little -if JTAG -speed 4000
 
 include $(GCC_AT91_MK_DIR)/dbg/jlink.mk
---
+++ mk/at91/at91sam7s-ek.mk
@@ -0,0 +1,27 @@
+# SPDX-License-Identifier: GPL-2.0+
+# Copyright (c) 2025 YOUNGJIN JOO (neoelec@gmail.com)
+
+AT91SAM7S_EK_MK_FILE	:= $(realpath $(lastword $(MAKEFILE_LIST)))
+AT91SAM7S_EK_MK_DIR	:= $(shell dirname $(AT91SAM7S_EK_MK_FILE))
+
+CHIP			?= at91sam7s256
+
+VPATH			+= $(AT91LIB)/boards/$(BOARD)
+VPATH			+= $(AT91LIB)/boards/$(PLATFORM)
+
+EXTRAINCDIRS		+= $(AT91LIB)/boards/$(BOARD)
+EXTRAINCDIRS		+= $(AT91LIB)/boards/$(PLATFORM)
+
+CSRCS			+= board_lowlevel.c
+CSRCS			+= board_memories.c
+
+ASRCS			+= board_cstartup.S
+
+BASE_flash		:= 0x100000
+BASE_sram		:= 0x200000
+
+CFLAGS			+= -mcpu=arm7tdmi
+CFLAGS			+= -mfloat-abi=soft
+
+# Linker Script
+LD_SCRIPT		?= $(AT91LIB)/boards/$(PLATFORM)/$(CHIP)/$(PROGMEM).lds
