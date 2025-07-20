d52a811c9593a938b6650fe29d8aef0f3f3fed91
YOUNGJIN JOO <neoelec@gmail.com>
2025-07-20 21:54:34 +0900

    arduino: fix bug, missing openocd.cfg for debugging R4 WiFi
    Signed-off-by: YOUNGJIN JOO <neoelec@gmail.com>

--- dbg/arduino/renesas_uno.mk
+++ dbg/arduino/renesas_uno.mk
@@ -1,6 +1,9 @@
 # SPDX-License-Identifier: GPL-2.0+
 # Copyright (c) 2024 YOUNGJIN JOO (neoelec@gmail.com)
 
+OPENOCDCFG		:= $(shell find $(ARDUINO_DIR) -name 'openocd.cfg' |\
+			grep UNOWIFIR4)
+
 include $(ARDUINO_CLI_MK_DIR)/dbg/inodbg.mk
 
 arduino.renesas_uno.minima.debug	:= jlinkgdb
