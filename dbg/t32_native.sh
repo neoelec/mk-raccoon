abeb914e1d373929c603f781c0a5ae333f327fba
YOUNGJIN JOO <neoelec@gmail.com>
2025-08-03 12:15:58 +0900

    dbg: rename to G_RCN_Target_Elf_File from G_RCN_Baremetal_Elf_File
    Signed-off-by: YOUNGJIN JOO <neoelec@gmail.com>

--- dbg/t32_native.sh
+++ dbg/t32_native.sh
@@ -11,7 +11,7 @@ ENTRY &E_Func %LINE &E_Args
 PRIVATE &ret
 &G_RCN_PWD=OS.PresentPracticeDirectory()
 ChDir &G_RCN_PWD
-&G_RCN_Baremetal_Elf_File="${DEBUG_SYMBOL}"
+&G_RCN_Target_Elf_File="${DEBUG_SYMBOL}"
 DO $(dirname $(realpath $0))/t32_native.cmm &E_Func &E_Args
 ENTRY &ret
 ENDDO &ret
