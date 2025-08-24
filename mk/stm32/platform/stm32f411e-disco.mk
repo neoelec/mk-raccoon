12063dbb5b8f4638d229e4e6a9cc5f221507a48e
YOUNGJIN JOO <neoelec@gmail.com>
2025-08-24 15:17:40 +0900

    stm32f411e-disco: fix typo errors
    Signed-off-by: YOUNGJIN JOO <neoelec@gmail.com>

--- mk/stm32/platform/stm32f411e-disco.mk
+++ mk/stm32/platform/stm32f411e-disco.mk
@@ -2,7 +2,7 @@
 # Copyright (c) 2025 YOUNGJIN JOO (neoelec@gmail.com)
 
 SERIES			:= STM32F4xx
-FAMILY			:= STM32F11xx
+FAMILY			:= STM32F411xE
 CHIP			:= STM32411vehx
 
 STM32CUBE_TEMPLATE	:= $(STM32CUBE_DIR)/Projects/STM32F411E-Discovery/Templates/STM32CubeIDE
